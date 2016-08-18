0d329bdf970e7b81d43b72a3d28bceab631cfaea
Johannes Lund <anviking@me.com>
2016-08-18 17:33:35 +0200

    Add operator overloads for Variable struct

--- Rydberg.playground/Contents.swift
+++ Rydberg.playground/Contents.swift
@@ -3,15 +3,15 @@
 import Cocoa
 
 let x = Variable(identifier: "x", value: 3)
-let ƒ: Expression = 1 + 3 * sin((2 * .variable(x)))
+let ƒ: Expression = 1 + 3 * sin((2 * x))
 let ƒ´ = ƒ.derive(withRespectTo: x)
 ƒ´.value
 
-let g: Expression = 2 * .variable(x) + 3
+let g: Expression = 2 * x + 3
 
 let y = Variable(identifier: "y", value: 3)
 
-let p: Expression = 4 * 2 ** 3 + 2 * 2 ** 2 - 4 * 2 + 1
+let p: Expression = 4 * x ** 3 + 2 * x ** 2 - 4 * x + 1
 
 var eq = Equation(lhs: .variable(y), rhs: g)
 eq.solve(for: x)
@@ -20,6 +20,6 @@ eq.lhs.derive(withRespectTo: x)
 
 let π = Expression.variable(Variable(identifier: "π", value: M_PI))
 
-LatexRenderer().render(expression: .variable(x) / (2 + 3) + 3 * π)
+LatexRenderer().render(expression: x / (2 + 3) + 3 * π)
 
 let a = 4.kg
---
+++ Rydberg.playground/Sources/Operators.swift
@@ -0,0 +1,93 @@
+import Foundation
+
+func ~= (lhs: Double, rhs: Expression) -> Bool {
+    return rhs.value == lhs
+}
+
+// MARK: Addition
+
+public func + (a: Expression, b: Expression) -> Expression {
+    return Expression.addition(a, b).optimized()
+}
+
+public func + (a: Variable, b: Expression) -> Expression {
+    return .addition(.variable(a), b)
+}
+
+public func + (a: Expression, b: Variable) -> Expression {
+    return .addition(a, .variable(b))
+}
+
+// MARK: Subtraction
+
+public func - (a: Expression, b: Expression) -> Expression {
+    return Expression.subtraction(a, b).optimized()
+}
+
+public func - (a: Variable, b: Expression) -> Expression {
+    return .subtraction(.variable(a), b)
+}
+
+public func - (a: Expression, b: Variable) -> Expression {
+    return .subtraction(a, .variable(b))
+}
+
+// MARK: Multiplication
+
+public func * (a: Expression, b: Expression) -> Expression {
+    return Expression.multiplication(a, b).optimized()
+}
+
+public func * (a: Variable, b: Expression) -> Expression {
+    return .multiplication(.variable(a), b)
+}
+
+public func * (a: Expression, b: Variable) -> Expression {
+    return .multiplication(a, .variable(b))
+}
+
+// MARK: Division
+
+public func / (a: Expression, b: Expression) -> Expression {
+    return Expression.division(a, b).optimized()
+}
+
+public func / (a: Variable, b: Expression) -> Expression {
+    return .division(.variable(a), b)
+}
+
+public func / (a: Expression, b: Variable) -> Expression {
+    return .division(a, .variable(b))
+}
+
+// MARK:
+
+precedencegroup PowerPrecedence {
+    higherThan: MultiplicationPrecedence, AdditionPrecedence
+}
+
+infix operator ** : PowerPrecedence
+
+public func ** (a: Expression, b: Expression) -> Expression {
+    return Expression.power(base: a, exponent: b)
+}
+
+public func ** (a: Variable, b: Expression) -> Expression {
+    return .power(base: .variable(a), exponent: b)
+}
+
+public func ** (a: Expression, b: Variable) -> Expression {
+    return Expression.power(base: a, exponent: .variable(b))
+}
+
+public func sqrt(_ a: Expression) -> Expression {
+    return .function(a, "sqrt", sqrt)
+}
+
+public func sin(_ a: Expression) -> Expression {
+    return .function(a, "sin", sin)
+}
+
+public func cos(_ a: Expression) -> Expression {
+    return .function(a, "cos", cos)
+}
